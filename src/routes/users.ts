0068a1f6a014ee53fd3e87455de684f01d940827
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-24 23:31:16 +0800

    Refactor endpoints with its auth

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
-import * as controller from "../controllers/users";
+import {
+  getAllUsers,
+  updatePassword,
+  updateRoleToAdmin,
+  updateUsername,
+} from "../controllers/users";
+import { isAuth, isAdmin, isOwner } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.get("/", controller.getAllUsers);
-router.get("/:id");
+router.get("/", isAuth, isAdmin, getAllUsers);
 
-router.post("/", controller.createUser);
-
-router.put("/username/:id", controller.updateUsername);
-router.put("/password/:id", controller.updatePassword);
+router.put("/:id/role/admin", isAuth, isOwner, updateRoleToAdmin);
+router.put("/username/:id", isAuth, updateUsername);
+router.put("/password/:id", isAuth, updatePassword);
 
 export default router;
