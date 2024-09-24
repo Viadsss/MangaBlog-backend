8d5be965ddba686c1608732807b2b324c4a47997
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-24 23:30:11 +0800

    Initialize passport and use auth strategy, and add another route for auth

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import passport from "passport";
 import usersRoute from "./routes/users";
-
-import errorHandler from "./middlewares/errorHandler";
+import authRoute from "./routes/auth";
+import errorHandler from "./middlewares/errorMiddleware";
+import { jwtStrategy } from "./middlewares/authMiddleware";
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(passport.initialize());
+
+passport.use(jwtStrategy);
+
+app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 
 app.use(errorHandler);
