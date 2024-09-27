037fafe200f0f863829b8bf4ba914d0fbbbe7472
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-28 01:02:14 +0800

    modified and move

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,18 +1,13 @@
 import { Router } from "express";
-import {
-  getAllUsers,
-  updatePassword,
-  updateRoleToAdmin,
-  updateUsername,
-} from "../controllers/users";
 import { isAuth, isAdmin, isOwner } from "../middlewares/authMiddleware";
+import * as controller from "../controllers/users";
 
 const router = Router();
 
-router.get("/", isAuth, isAdmin, getAllUsers);
+router.get("/", controller.getAllUsers);
 
-router.put("/:id/role/admin", isAuth, isOwner, updateRoleToAdmin);
-router.put("/username/:id", isAuth, updateUsername);
-router.put("/password/:id", isAuth, updatePassword);
+router.put("/:id/role/admin", isAuth, isOwner, controller.updateRoleToAdmin);
+router.put("/username/:id", isAuth, controller.updateUsername);
+router.put("/password/:id", isAuth, controller.updatePassword);
 
 export default router;
