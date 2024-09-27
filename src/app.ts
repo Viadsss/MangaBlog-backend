23563c171c954a1a1801b4cecba46c9fb50c757f
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-28 00:58:04 +0800

    Add posts route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import passport from "passport";
-import usersRoute from "./routes/users";
-import authRoute from "./routes/auth";
 import errorHandler from "./middlewares/errorMiddleware";
 import { jwtStrategy } from "./middlewares/authMiddleware";
+import authRoute from "./routes/auth";
+import usersRoute from "./routes/users";
+import postsRoute from "./routes/posts";
 
 const app = express();
 
@@ -14,6 +15,7 @@ passport.use(jwtStrategy);
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
 
 app.use(errorHandler);
 
