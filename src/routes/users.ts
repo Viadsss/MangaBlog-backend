bdfd2a4661c5c3c3a6c9eae7d4e4c89f99cff490
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-29 00:39:50 +0800

    Refactor

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,10 +4,10 @@ import * as controller from "../controllers/users";
 
 const router = Router();
 
-router.get("/", controller.getAllUsers);
+router.get("/", isAuth, isOwner, controller.getAllUsers);
 
+router.put("/:id", isAuth, controller.updateUser);
+router.put("/:id/password", isAuth, controller.updatePassword);
 router.put("/:id/role/admin", isAuth, isOwner, controller.updateRoleToAdmin);
-router.put("/username/:id", isAuth, controller.updateUsername);
-router.put("/password/:id", isAuth, controller.updatePassword);
 
 export default router;
