b30c178bb65f8f5d5589d3906ca6acf942e69693
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-30 23:04:35 +0800

    Add update post controlleR

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -18,6 +18,16 @@ const validateCreatePost = [
   body("content").notEmpty().withMessage("Content is required"),
 ];
 
+const validateUpdatePost = [
+  body("title")
+    .optional()
+    .trim()
+    .isLength({ min: 5, max: 100 })
+    .withMessage("Title must be between 5 and 100 characters long"),
+
+  body("content").optional(),
+];
+
 export const getAllPosts = async (
   req: Request,
   res: Response,
@@ -68,6 +78,57 @@ export const createPost = [
       }
 
       res.status(201).json(newPost);
+    } catch (err) {
+      next(err);
+    }
+  },
+];
+
+export const updatePost = [
+  upload.single("bannerImage"),
+  ...validateUpdatePost,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const bannerImage = req.file;
+      const id = parseInt(req.params.id);
+      const { title, content } = req.body;
+
+      const post = await postService.getPostById(id);
+      if (!post) throw new NotFoundError("Post not found");
+
+      let updated = false;
+      const updatedData: { title?: string; content?: string } = {};
+
+      if (title && title !== post.title) {
+        updatedData.title = title;
+        updated = true;
+      }
+
+      if (content && content !== post.content) {
+        updatedData.content = content;
+        updated = true;
+      }
+
+      if (bannerImage) {
+        const bannerUrl = await cloudinaryService.uploadBannerImage(
+          id,
+          bannerImage
+        );
+        await postService.updateBannerUrl(id, bannerUrl);
+        updated = true;
+      }
+
+      if (!updated) {
+        return res.json({ message: "No changes made", updated: false });
+      }
+
+      await postService.updatePost(id, updatedData);
+      res.json({ message: "Post updated successfully", updated: true });
     } catch (err) {
       next(err);
     }
