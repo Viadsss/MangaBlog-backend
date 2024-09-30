d1d1c449e1859cd4e22fff95e7284b47a8c5bcdc
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-09-30 23:04:49 +0800

    Add update post route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { isAuth } from "../middlewares/authMiddleware";
+import { isAuth, isAdmin } from "../middlewares/authMiddleware";
 import * as controller from "../controllers/posts";
 
 const router = Router();
 
 router.get("/", controller.getAllPosts);
 
-router.post("/:authorId", isAuth, controller.createPost);
+router.post("/:authorId", isAuth, isAdmin, controller.createPost);
+
+router.put("/:id", isAuth, isAdmin, controller.updatePost);
 
 export default router;
